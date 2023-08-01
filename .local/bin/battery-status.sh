826e557cf87c3806c94f9387f7cc774ba7f9e25e
usernyan <74884883+usernyan@users.noreply.github.com>
2023-08-01 11:10:03 -0400

    updated battery script

--- .local/bin/battery-status.sh
+++ .local/bin/battery-status.sh
@@ -1,6 +1,8 @@
+#!/bin/sh
+
 for b in /sys/class/power_supply/BAT?
 do
   capacity="$(cat "$b"/capacity)"
   status="$(cat "$b"/status)"
-  printf "%s:%s%%" "$status" "$capacity"
+  printf "%s:%s:%s%%" "$(basename "$b" )" "$status" "$capacity"
 done
