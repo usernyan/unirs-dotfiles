564a88a1af6039844c0b66e28e1e0393c6568e75
usernyan <74884883+usernyan@users.noreply.github.com>
2023-08-03 17:40:34 -0400

    fixed cpupower script

--- .local/bin/cpupower-dmenu.sh
+++ .local/bin/cpupower-dmenu.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 governors="$(cpupower frequency-info | sed --quiet '/available cpufreq governors/p' | sed 's/^.*: //' )"
-user_choice="$(printf "%s\n" "$governors" | sed 's/ /\n/g' | dmenu)"
-#Note: give the user perms to run cpupower without password
+user_choice="$(printf "%s\n" "$governors" | sed 's/ /\n/g' | dmenu -p "What CPU Freq Governor?" -i)"
+test -n "$user_choice" || exit
+#Note: give the user perms to run cpupower without a password
 sudo cpupower frequency-set --governor "$user_choice"
 notify-send "CPU Freq Governor:$user_choice"
 
