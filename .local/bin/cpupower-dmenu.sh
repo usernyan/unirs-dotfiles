2b2172328b6fb4a518be654d25a339ccb0753655
usernyan <74884883+usernyan@users.noreply.github.com>
2023-08-03 17:52:18 -0400

    restricted cpupower userchoice

--- .local/bin/cpupower-dmenu.sh
+++ .local/bin/cpupower-dmenu.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 governors="$(cpupower frequency-info | sed --quiet '/available cpufreq governors/p' | sed 's/^.*: //' )"
-user_choice="$(printf "%s\n" "$governors" | sed 's/ /\n/g' | dmenu -p "What CPU Freq Governor?" -i)"
+user_choice="$(printf "%s\n" "$governors" | sed 's/ /\n/g' | dmenu -p "What CPU Freq Governor?" -i -r)"
 test -n "$user_choice" || exit
 #Note: give the user perms to run cpupower without a password
 sudo cpupower frequency-set --governor "$user_choice"
