5af662332d605d158cecf4a808faebc22b6791a8
usernyan <74884883+usernyan@users.noreply.github.com>
2023-09-02 11:17:36 -0400

    Added udisks mounting scripts

---
+++ .local/bin/unmount-udisk.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# 1: get available block devices
+BLOCK_DEVS="$(udisksctl dump | grep "^\s*Device:" | sed 's/Device://;s/\s//g')"
+# 2: ask the user for a choice
+USER_DEV="$(printf "$BLOCK_DEVS" | dmenu)"
+# 3: mount the user's device
+udisksctl unmount -b "$USER_DEV"
