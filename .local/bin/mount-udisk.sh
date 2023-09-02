4f78b5644f184737cdf366ad5700f4df96f75279
usernyan <74884883+usernyan@users.noreply.github.com>
2023-09-02 18:42:37 -0400

    (un)mounting scripts ignore no input

--- .local/bin/mount-udisk.sh
+++ .local/bin/mount-udisk.sh
@@ -4,7 +4,7 @@
 BLOCK_DEVS="$(udisksctl dump | grep "^\s*Device:" | sed 's/Device://;s/\s//g')"
 # 2: ask the user for a choice
 USER_DEV="$(printf "$BLOCK_DEVS" | dmenu -p "Mount:")"
+[ "$USER_DEV" == "" ] && exit 0
 # 3: mount the user's device
-udisksctl mount -b "$USER_DEV"
-
-#TODO: notify-send errors from udiskctl
+MESSAGE="$(udisksctl mount -b "$USER_DEV" 2>&1)"
+notify-send "$MESSAGE"
--- .local/bin/unmount-udisk.sh
+++ .local/bin/unmount-udisk.sh
@@ -3,6 +3,8 @@
 # 1: get available block devices
 BLOCK_DEVS="$(udisksctl dump | grep "^\s*Device:" | sed 's/Device://;s/\s//g')"
 # 2: ask the user for a choice
-USER_DEV="$(printf "$BLOCK_DEVS" | dmenu -p "Unmount:")"
+USER_DEV="$(printf "$BLOCK_DEVS" | dmenu -p "Mount:")"
+[ "$USER_DEV" == "" ] && exit 0
 # 3: mount the user's device
-udisksctl unmount -b "$USER_DEV"
+MESSAGE="$(udisksctl unmount -b "$USER_DEV" 2>&1)"
+notify-send "$MESSAGE"
